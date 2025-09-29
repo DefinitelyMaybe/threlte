241c12818d62bfaf84bc83fd7ce6a74fb30af87e
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-09-29 17:56:03 +1300

    make a random particles page

---
+++ apps/docs/src/examples/extras/particles/campfire/Scene.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
+  import Fire from './fire.svelte'
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[5, 5, 5]}
+>
+  <OrbitControls />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight
+  position.y={10}
+  position.z={5}
+/>
+
+<T.AmbientLight intensity={0.1} />
+
+<T.Mesh>
+  <T.BoxGeometry />
+  <T.MeshStandardMaterial
+    transparent={true}
+    opacity={0.1}
+  />
+</T.Mesh>
+
+<Fire />
---
+++ apps/docs/src/examples/extras/particles/campfire/fire.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { T, useTask } from '@threlte/core'
+  import { Instance, InstancedMesh } from '@threlte/extras'
+
+  interface FireProps {
+    count: number
+  }
+  let { count = 10 }: FireProps = $props()
+
+  let dn = $state(Date.now())
+  useTask(() => (dn = Date.now()))
+</script>
+
+<InstancedMesh>
+  <T.SphereGeometry args={[0.5]} />
+  <T.MeshStandardMaterial color="white" />
+
+  {#each { length: count } as _, i}
+    <Instance
+      position.x={Math.random() * i}
+      position.y={Math.sin(dn / 1000 + 40)}
+    />
+  {/each}
+</InstancedMesh>
