7516d54da8962764e1de679fb75ef879263e79d3
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-10-07 13:59:00 +1300

    add initial pages and deps for docs

---
+++ apps/docs/src/examples/vfx/dissolve/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Canvas, extend } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import { WebGPURenderer } from 'three/webgpu'
+  import * as THREE from 'three/webgpu'
+  extend(THREE)
+</script>
+
+<div>
+  <Canvas
+    createRenderer={(canvas) => {
+      return new WebGPURenderer({
+        canvas,
+        antialias: true,
+        forceWebGL: false
+      })
+    }}
+  >
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    height: 100%;
+  }
+</style>
