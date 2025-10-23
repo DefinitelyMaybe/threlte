944c462bc4b8e1f95adce5242d9ab247a74dc5e9
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-10-24 12:19:12 +1300

    init attempt at outline postprocessing using tsl

---
+++ apps/docs/src/examples/vfx/outline/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import CustomRenderer from './CustomRenderer.svelte'
+  import Scene from './Scene.svelte'
+  import { Canvas, extend } from '@threlte/core'
+  import { WebGPURenderer } from 'three/webgpu'
+  import * as THREE from 'three/webgpu'
+  extend(THREE)
+</script>
+
+<Canvas
+  createRenderer={(canvas: HTMLCanvasElement) => {
+    return new WebGPURenderer({
+      canvas,
+      antialias: true
+    })
+  }}
+>
+  <Scene />
+  <CustomRenderer />
+</Canvas>
---
+++ apps/docs/src/examples/vfx/outline/Scene.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import type { Object3D } from 'three'
+  import { CameraControls, interactivity, type IntersectionEvent } from '@threlte/extras'
+  import { T } from '@threlte/core'
+  import { state } from './state.svelte'
+
+  interactivity()
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+>
+  <CameraControls />
+</T.PerspectiveCamera>
+
+<T.DirectionalLight />
+<T.AmbientLight />
+
+<T.Mesh
+  onclick={(ref: IntersectionEvent<Object3D>) => {
+    state.selectedObjects = [ref.object]
+  }}
+>
+  <T.MeshStandardMaterial color="gold" />
+  <T.BoxGeometry />
+</T.Mesh>
+
+<T.Mesh
+  scale={10}
+  position.y={-0.51}
+  rotation.x={-Math.PI / 2}
+>
+  <T.PlaneGeometry />
+  <T.MeshStandardMaterial color="green" />
+</T.Mesh>
