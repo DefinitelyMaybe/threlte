48024fc5bdf9d7410452f2aacd118843a16c2559
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2025-10-24 12:17:13 +1300

    add vfx test example

---
+++ apps/docs/src/examples/vfx/test/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Canvas, extend } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import { WebGPURenderer } from 'three/webgpu'
+  import * as THREE from 'three/webgpu'
+  extend(THREE)
+</script>
+
+<div>
+  <Canvas
+    createRenderer={(canvas) => {
+      return new WebGPURenderer({
+        canvas,
+        antialias: true
+      })
+    }}
+  >
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  div {
+    height: 100%;
+  }
+</style>
